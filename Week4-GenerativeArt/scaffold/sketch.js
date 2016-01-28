d5ae4a6c8c3e6e1033eec03afd68ca2493f81d50
Ricki@mac <rickixie@Rickis-MacBook-Pro-2.local>
2016-01-28 11:24:58 -0800

    update scaffold code for ex4, week4

---
+++ Week4-GenerativeArt/scaffold/sketch.js
@@ -0,0 +1,14 @@
+// Define any global variables here
+// (BUT don't call any p5 methods here;
+//  call them in the functions below!)
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); // Use the full browser window
+  // Additional setup goes here
+}
+
+function draw() {
+  // Put your drawing code here
+}
+
+// Define any additional helper functions here
