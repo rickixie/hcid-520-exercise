a692cda4bd460ab0e8d0bdfa8730c01261b7c2d8
Ricki@mac <rickixie@Rickis-MacBook-Pro-2.local>
2016-02-07 21:07:09 -0800

    update interactive art

--- Week5-InteractiveArt/sketch.js/archive/sketch-bouncingball-same-color.js
+++ Week5-InteractiveArt/sketch.js/archive/sketch-bouncingball-same-color.js
@@ -8,74 +8,25 @@ function setup() {
   for (var i=0; i<40; i++){
     var newBall = new Ball();//create a new ball
     ballArray[i] = newBall;//put into the array
-
+    // changeAllColor();
+    // clear();
   }
   frameRate(20);
+
 }
 
 function draw() {
   background('black');
 
-  var count = 0;
+  
   for (var i=0; i<ballArray.length; i++){
     ballArray[i].bounce();
+    ballArray[i].changeColor();
     ballArray[i].move();
     ballArray[i].display();
-    count++;
-  
   }
 
-}
 
-function onKeyDown(e){
-   switch(e.keyCode){
-     case:49 {//Number: 1
-       
-       break;
-     }
-     case: 50 {//Number:2
-       
-       break;
-     }
-     case: 51 {//Number:3
-       
-       break;
-     }
-     case:52 {//Number: 4
-       
-       break;
-     }
-     case:53 {//Number: 5
-       
-       break;
-     }
-     case: 54 {//Number: 6
-       
-       break;
-     }
-     case:55 {//Number: 7
-       
-       break;
-     }
-     case: 56 {//Number: 8
-       
-       break;
-     }
-     case: 57 {//Number: 9
-       
-       break;
-     }
-      case: 32 {//Number: space
-       
-       break;
-     }
-      case: 27 {//Number: escape
-       
-       break;
-     }
-     
-    
-   }
 }
 
 
@@ -83,6 +34,7 @@ function changeAllColor(){
     var r = floor(random(255));
     var g = floor(random(255));  
     var b = floor(random(255));
+
     fill('rgba('+r+','+g+','+b+',0.5)');
     noStroke();
 }
@@ -93,11 +45,6 @@ function Ball() {
   this.x = random(width);
   this.y = random(height);
   this.diameter = random(50, 100);
-  var r = floor(random(255));
-  var g = floor(random(255));  
-  var b = floor(random(255));
-
-  this.color = color('rgba('+r+','+g+','+b+',0.5)');
   
   this.speed = 1;
  
@@ -108,12 +55,19 @@ function Ball() {
 
   };
     
-  this.display = function() {
-    fill(this.color);//only fill the color when you create the ball
-    noStroke();
+    this.display = function() {
     ellipse(this.x, this.y, this.diameter, this.diameter);
   };
 
+
+  this.changeColor = function(){
+    var r = floor(random(255));
+    var g = floor(random(255));  
+    var b = floor(random(255));
+    fill('rgba('+r+','+g+','+b+',0.5)');
+    noStroke();
+
+  }
 
   var speedY = 20;
   var speedX = 20;
